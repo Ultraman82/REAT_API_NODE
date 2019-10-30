2f6334203932e297f12d7dfca9b343b23c738481
Ultrman82 <exelcior99@gmail.com>
2019-10-29 23:12:29 -0700

    #6 Mongoose MongoDB

--- app.js
+++ app.js
@@ -1,41 +1,52 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const productRoutes = require('./api/routes/products');
-const orderRoutes = require('./api/routes/orders');
+const productRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/orders");
 
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
+mongoose.connect(
+  `mongodb://${process.env.MONGO_ID}:${process.env.MONGO_PW}@ds233288.mlab.com:33288/graphql`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Header',
-     'Origin, X-Requested-Woth, Content-Type, Accept, Authorization');
-     if (req.method === 'OPTIONS') {
-         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-         return res.status(200).json({});
-     }
-     next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Header",
+    "Origin, X-Requested-Woth, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
 });
 
-app.use('/products', productRoutes);
-app.use('/orders', orderRoutes);
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
 app.use((req, res, next) => {
-    const error = new Error('Not found');
-    error.status(404);
-    next(error)
+  const error = new Error("Not found");
+  error.status(404);
+  next(error);
 });
 
 app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.json({
-        error: {
-            message: error.message
-        }
-    })
-})
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
 
 module.exports = app;
