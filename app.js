c3fbabcd78458e09272d002257676f2256cd9a7a
Ultrman82 <exelcior99@gmail.com>
2019-10-30 22:10:08 -0700

    #11 Adding User Signup

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose");
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+const userRoutes = require("./api/routes/user");
 
 mongoose.connect(
   `mongodb://${process.env.MONGO_ID}:${process.env.MONGO_PW}@ds233288.mlab.com:33288/graphql`,
@@ -39,6 +40,7 @@ app.use((req, res, next) => {
 
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+app.use("/user", userRoutes);
 app.use((req, res, next) => {
   const error = new Error("Not found");
   error.status(404);
